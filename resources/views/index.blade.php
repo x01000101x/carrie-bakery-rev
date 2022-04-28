ac805537c725d5271402c06b189a90d102e85f26
x01000101x <leodamanik619@gmail.com>
2022-04-28 14:36:07 +0700

    checkpoint

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -254,7 +254,7 @@
 		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 		      	<ul class="nav navbar-nav nav-main">
 		        	<li class="active"><a href="#">HOME</a></li>
-					<li><a href="products.html">SHOP</a></li>
+					<li><a href="{{ url('shop'); }}">SHOP</a></li>
 					<li><a href="blog.html">BLOG</a></li>
 					<li><a href="blog-single.html">ARTICLE</a></li>
 					<li class="dropdown">
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RotiController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/shop', [RotiController::class, 'show'])->name('shop');
 
 Route::middleware([
     'auth:sanctum',
