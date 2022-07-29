13c2dfe6626fe11ae016bdbbaf737660f2dcb0a6
x01000101x <leodamanik619@gmail.com>
2022-07-29 14:01:02 +0700

    finish print invoice Firewall788

--- resources/views/confirm.blade.php
+++ resources/views/confirm.blade.php
@@ -339,7 +339,14 @@ body{
     document.getElementById("printout").addEventListener("click", () =>{
         const invoice = this.document.getElementById("sangiang");
         console.log(invoice);
-        html2pdf().from(invoice).save();
+        var opt = {
+            margin:       1,
+            filename:     'carriebakery-invoice.pdf',
+            image:        { type: 'jpeg', quality: 0.98 },
+            html2canvas:  { scale: 2 },
+            jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+            };
+        html2pdf().from(invoice).set(opt).save();
     })
    }
 </script>
